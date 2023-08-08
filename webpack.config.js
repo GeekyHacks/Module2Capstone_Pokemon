0e4470f3fe891a627866ee75e9cccfc513f32314
GeekyHacks <geekyhacks22@gmail.com>
2023-08-08 12:33:45 +0300

    Add webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
   mode: 'development',
@@ -56,6 +55,5 @@ module.exports = {
       filename: 'index.html',
       template: 'src/template.html',
     }),
-    new BundleAnalyzerPlugin(),
   ],
 };
